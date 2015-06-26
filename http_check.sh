986eed164a51963222320fdbba0de06c2bac4900
Ricardo <ricmmart@gmail.com>
2015-06-26 14:45:04 +0000

    Modificado comando do wget

--- http_check.sh
+++ http_check.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#/tmp/http_check.sh
-if [ "$(wget --spider -S "http://localhost" 2>&1 | grep "HTTP/" | awk '{print $2}')" = "200" ]; then
+if [ "$(wget --spider -S "http://localhost" 2>&1 > /dev/null | grep "HTTP/" | awk '{print $2}')" = "200" ]; then
  exit 0
   else
  exit 1
