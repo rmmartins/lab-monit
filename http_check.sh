d36124fbfd2cecd3df59240f4d021707c234e138
Ricardo <ricmmart@gmail.com>
2015-06-26 15:02:59 +0000

    Melhoria na mensagem de erro

--- http_check.sh
+++ http_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ "$(wget --spider -S "http://localhost" 2>&1 > /dev/null | grep "HTTP/" | awk '{print $2}')" = "200" ]; then
+if [ "$(wget --spider -S "http://localhost" 2>&1 | grep "HTTP/" | awk '{print $2}')" = "201" ]; then
+ echo "HTTP response is OK"
  exit 0
   else
+ echo "HTTP response with error"
  exit 1
 fi
